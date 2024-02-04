0f0721a5cc54dfe89aaa3c1e377aba6ff9203687
frontendmoe <frontendmoe@gmail.com>
2024-02-04 20:36:49 +0100

    StyleUpdates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,29 @@
-import { Providers } from './providers'
-
+import { Providers } from './providers';
+import '../index.css';
 export const metadata = {
-  title: 'zkSync + wagmi + Next.js',
-}
+	title: 'zkSync + wagmi + Next.js',
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
+	const globalStyles: React.CSSProperties = {
+		margin: 0,
+		padding: 0,
+		fontFamily: 'sans-serif',
+		fontSize: '16px',
+		// color: '#ffffff',
+		lineHeight: '1',
+		// backgroundColor: '#000000',
+	};
 
-  const globalStyles: React.CSSProperties = {   
-    margin: 0,
-    padding: 0,
-    fontFamily: 'sans-serif',
-    fontSize: '16px',
-    // color: '#ffffff',
-    lineHeight: '1',
-    // backgroundColor: '#000000',
-  }
-
-  return (
-    <html lang="en">
-      <body style={globalStyles}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body style={globalStyles}>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
